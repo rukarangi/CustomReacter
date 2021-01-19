4c6a0dde2aea80d026fff424e3cdc926c2949a95
Luke <luke@yorkbay.net.nz>
2021-01-19 23:25:08 +1300

    Added useful help and rework msg handler

--- bot.py
+++ bot.py
@@ -1,7 +1,14 @@
 import discord
 import os
+import re
 
 client = discord.Client()
+
+print(re.search("\$\w+\s*", "$helo"))
+
+# store key on local machine
+with open("key.txt") as f:
+	key = f.read()
 
 @client.event
 async def on_ready():
@@ -14,12 +21,77 @@ async def on_message(message):
 
 	print(message.author)
 
-	#if message.content.startswith("$hello"):
+	if message.content.startswith("$"):
+		await messageHandler(message)
+	else:
+		if message.author.id == 276190242799616001:
+			print("its luke")
+			await message.add_reaction("💩")
+			print(message.channel.permissions_for(message.author).administrator)
+
+		if message.author.id == 471977641986228224:
+			print("its pepep")
+			await message.add_reaction("🇺")
+			await message.add_reaction("🇷")
+			await message.add_reaction("💩")
+
+
+
+
+async def displayCommands(channel, m):
+	embedMsg = discord.Embed(title="", description="List of Commmands:", color=0x00ff00)
+	for key, com in m.items():
+		embedMsg.add_field(name="$"+key, value=str(com.__doc__), inline=False)
+	await channel.send(embed=embedMsg)
+
+
+async def hello(message, m):
+	"""
+	Basic hello response for testing
+	"""
+	await message.channel.send("hello")
+	await message.add_reaction("😀") 
+	return
+
+async def info(message, m):
+	"""
+	This displays this
+	"""
+	#await message.channel.send("This bot is a test of some discord.py functionalities: use $hello to see a reaction.")
+	await displayCommands(message.channel, m)
+	return
+
+async def badCommand(message, m):
+	await message.channel.send("That is not a command i recognise, try '$help'")
+	return
+
+messages = {
+	"hello": hello,
+	"help": info
+}
+
+
+async def messageHandler(message):
+	content = message.content
+	author = message.author
+	admin = message.channel.permissions_for(author).administrator
+	
+	#print(f'{author} admin:({admin}) : {content}')
+
+	command = message.content[1:5]
+	#print(re.search("\$\w+\s*", content))
+	command = re.search("\$\w+\s*", content).group()[1:]
+	#print(command)
+	command = "".join([s for s in command if s != " "])
+	#print(command)
+
+	func = messages.get(command, badCommand)
+	await func(message, messages)
+
+
+	#if message.content[1:5] == "hello":
 	#	await message.channel.send("hello")
-	#s	await message.add_reaction("😀")
+	#	await message.add_reaction("😀")
+	return
 
-	if message.author == "thukeeo#8811":
-		print("its luke")
-		await message.add_reaction("💩")
-
-client.run("NzkzMDczOTUwOTcxNTI3MTY5.X-m9rA.kjKh6jf4fyyifMY0QGixkFnEtiQ")
+client.run(key)
